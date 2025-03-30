113693bbf83e389f3ff8e82d1e5dd93ee36cfad4
eduardo <eduardozaffarimonteiro@gmail.com>
2025-03-30 12:53:44 -0300

    primeira rnn funcional

--- utils.py
+++ utils.py
@@ -1,16 +1,40 @@
 import pandas as pd
+import re
 
-def LoadAndTransform(seqs="train_sequences.csv", labels="train_labels.csv"):
+def Load(seqs="train_sequences.csv", labels="train_labels.csv"):
+
     train_seqs = pd.read_csv(seqs)
 
-    train_seqs_new = train_seqs.copy()
-    train_seqs_new["resname"] = train_seqs_new["sequence"].apply(lambda x: list(x))
-    train_seqs_new = train_seqs_new[["target_id", "sequence", "resname"]].explode("resname", ignore_index=True)
-    train_seqs_new["resid"] = train_seqs_new.groupby("target_id").cumcount() + 1
-    train_seqs_new["target_id"] = train_seqs_new["target_id"] + "_" + train_seqs_new["resid"].astype(str)
+    train_seqs = train_seqs[~train_seqs["sequence"].str.contains("X")
+        & ~train_seqs["sequence"].str.contains("-")].reset_index(drop=True)
+    train_seqs = train_seqs[["target_id","sequence"]]
+
+    train_seqs = train_seqs[train_seqs["sequence"].str.len() > 20]
+
+    train_seqs["sequence"] = train_seqs["sequence"].str.strip()
+    train_seqs["sequence"] = train_seqs["sequence"].str.replace("G", "1")
+    train_seqs["sequence"] = train_seqs["sequence"].str.replace("C", "2")
+    train_seqs["sequence"] = train_seqs["sequence"].str.replace("A", "3")
+    train_seqs["sequence"] = train_seqs["sequence"].str.replace("U", "4")
+
+    train_seqs["sequence"] = train_seqs["sequence"].apply(lambda sequence: [int(molecule) for molecule in sequence])
+    train_seqs = train_seqs.explode("sequence")
+
+    train_seqs["resid"] = train_seqs.groupby("target_id").cumcount() + 1
+    train_seqs["target_num"] = train_seqs["target_id"] + "_" + train_seqs["resid"].astype(str)
 
     train_labels = pd.read_csv(labels)
+    train_labels["target_id"] = train_labels["ID"].apply(lambda x: re.split("_\d+", x)[0])
+    train_labels = train_labels.groupby("target_id").filter(lambda x: ~(x.isna().any().any()))
+    train_labels = train_labels.drop("target_id", axis=1)
 
-    train_data = pd.merge(train_seqs_new[["target_id", "sequence"]], train_labels, left_on="target_id", right_on="ID").drop("target_id", axis=1)
+
+    train_data = pd.merge(train_seqs, train_labels, left_on="target_num", right_on="ID", how="left").drop(["target_num","resid_x"], axis=1)
+
+    train_data = train_data.rename({"resid_y": "resid", "sequence": "molecule"}, axis=1)
+
+    train_data = train_data.dropna()
+
+    train_data = train_data[train_data["resid"] <= 21]
 
     return train_data
